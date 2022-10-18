ceaff20e79d8157d6648a3a46cdab30c2cdd137f
Thanh Ly <thanhgly.se@gmail.com>
2022-10-17 17:53:18 -0700

    Complete react components

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // TODO
+const { useState } = React;
+const GroceryList = (props) => (
+  <div>
+    <h2>Grocery List</h2>
+    <ul>
+      {props.groceryItems.map((item) => <GroceryListItem item = {item}/>)}
+    </ul>
+  </div>
+);
+
+const GroceryListItem = (props) => {
+  const [isDone, setIsDone] = useState(false);
+  const onMouseOver = () => setIsDone(!isDone);
+  const style = {fontWeight: isDone ? 'bold' : 'normal'};
+  return (
+  <li style={style} onMouseOver={onMouseOver}>{props.item}</li>
+  )
+};
+
+ReactDOM.render(<GroceryList groceryItems = {['Eggs', 'Milk', 'Cheese']}/>, document.getElementById('app'));
