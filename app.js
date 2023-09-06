6211edbc23cb036558f44d8bec09f3e3906f9ce5
hoanhua14 <huasohoan@gmail.com>
2023-09-05 23:18:57 -0700

    style changed

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // TODO
+const {useState} = React
+const GroceryList = (props) => {
+
+    const [status, setStatus] = useState(false);
+
+    const styleChange = {
+        'text-decoration': status ? 'line-through' : 'none',
+    }
+
+    const onListItemClick = () => {
+        setStatus(!status);
+        console.log(status);
+
+    }
+
+    return (
+        <ul>
+            <li onClick={onListItemClick} style={styleChange}>{props.groceryItems[0]}</li>
+            <li>{props.groceryItems[1]}</li>
+
+        </ul>
+    )
+}
+const App = () => {
+    const groceryItems = ['watermelon', 'orange', 'banana']
+    return (
+        <>
+            <div>My grocery list</div>
+            <GroceryList groceryItems={groceryItems}/>
+        </>)
+}
+
+
+
+ReactDOM.render(<App/>, document.getElementById('app'))
