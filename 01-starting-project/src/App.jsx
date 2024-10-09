94553fad93f983970e7b99da3b2be61be87e46b2
hoanhua14 <huasohoan@gmail.com>
2024-10-08 21:31:56 -0700

    button renders dynamic content

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,8 +2,9 @@ import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 function App() {
-  function handleSelect() {
-    console.log("clicked");
+  let text = "blah";
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
   }
   return (
     <div>
@@ -18,12 +19,13 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          Dynamic content
         </section>
         <h2>Time to get started!</h2>
       </main>
