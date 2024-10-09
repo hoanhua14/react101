cc2b04adef241aa9c5adda4808a9014fb1eaee78
hoanhua14 <huasohoan@gmail.com>
2024-10-08 23:23:54 -0700

    menu renders dynamically

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
+import { EXAMPLES } from "./data.js";
 function App() {
-  let text = "blah";
-  function handleSelect(selectedButton) {
-    console.log(selectedButton);
+  const [selected, setSelected] = useState("components");
+  function handleSelect(selected) {
+    setSelected(selected);
   }
   return (
     <div>
@@ -26,6 +28,11 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selected].title}</h3>
+            <p>{EXAMPLES[selected].description}</p>
+            <pre>{EXAMPLES[selected].code}</pre>
+          </div>
         </section>
         <h2>Time to get started!</h2>
       </main>
