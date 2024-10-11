dd7bceee67e07af45fe6d2498936aa2493512d4b
hoanhua14 <huasohoan@gmail.com>
2024-10-10 19:03:31 -0700

    splitted components based on state

--- 01-starting-project/src/components/CoreConcept.jsx
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -1,16 +1,3 @@
-import { CORE_CONCEPTS } from "../data";
 export default function CoreConcept() {
-  return (
-    <div>
-      {CORE_CONCEPTS.map((concept) => {
-        return (
-          <li key={concept.title}>
-            <img src={concept.image} alt={concept.title}></img>
-            <h1>{concept.title}</h1>
-            <p>{concept.description}</p>
-          </li>
-        );
-      })}
-    </div>
-  );
+  return;
 }
---
+++ 01-starting-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,17 @@
+import { CORE_CONCEPTS } from "../data";
+export default function CoreConcepts() {
+  return (
+    // <div>
+    //   {CORE_CONCEPTS.map((concept) => {
+    //     return (
+    //       <li key={concept.title}>
+    //         <img src={concept.image} alt={concept.title}></img>
+    //         <h1>{concept.title}</h1>
+    //         <p>{concept.description}</p>
+    //       </li>
+    //     );
+    //   })}
+    // </div>
+
+  );
+}
