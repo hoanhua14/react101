faa811f2e9b55fa7d4c9e4451ba06d234a30dec2
hoanhua14 <huasohoan@gmail.com>
2024-12-10 17:00:52 -0800

    dynamic player name

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -2,20 +2,23 @@ import { useState } from "react";
 
 export default function Player({ player, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  let playerName = <span className="player-name">{player}</span>;
+  let buttonName = "Edit";
   const handleEditClicking = () => {
     setIsEditing(true);
   };
-  let playerName = <span className="player-name">{player}</span>;
   if (isEditing) {
+    buttonName = "Save";
     playerName = <input type="text" required />;
   }
+
   return (
     <li>
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClicking}>Edit</button>
+      <button onClick={handleEditClicking}>{buttonName}</button>
     </li>
   );
 }
