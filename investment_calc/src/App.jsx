f498ce98e070bfe7405ba727c3595fe4fdefeea7
hoanhua14 <huasohoan@gmail.com>
2025-01-05 17:35:07 -0800

    created Result and UserInput components

--- investment_calc/src/App.jsx
+++ investment_calc/src/App.jsx
@@ -1,8 +1,56 @@
+import { useState } from "react";
 import Header from "./util/Header";
+import Result from "./util/Result";
 function App() {
+  const [initialInvestment, setInitialInvestment] = useState("");
+  const [annualInvestment, setAnnualInvestment] = useState("");
+  const [expectedReturn, setExpectedReturn] = useState("");
+  const [duration, setDuration] = useState("");
+  const handleInitialInvestmentChange = (e) => {
+    setInitialInvestment(e.target.value);
+    console.log(initialInvestment);
+  };
+  const handleAnnualInvestmentChange = (e) => {
+    setAnnualInvestment(e.target.value);
+    console.log(annualInvestment);
+  };
+  const handleExpectedReturnChange = (e) => {
+    setExpectedReturn(e.target.value);
+    console.log(expectedReturn);
+  };
+  const handleDurationChange = (e) => {
+    setDuration(e.target.value);
+    console.log(duration);
+  };
   return (
     <>
       <Header />
+      <div id="user-input" className="input-group">
+        <label>initial investment</label>
+        <input
+          value={initialInvestment}
+          onChange={handleInitialInvestmentChange}
+        ></input>
+        <label>annual investment</label>
+        <input
+          value={annualInvestment}
+          onChange={handleAnnualInvestmentChange}
+        ></input>
+
+        <label>expected return</label>
+        <input
+          value={expectedReturn}
+          onChange={handleExpectedReturnChange}
+        ></input>
+        <label>duration</label>
+        <input value={duration} onChange={handleDurationChange}></input>
+      </div>
+      <Result
+        initialInvestment={initialInvestment}
+        annualInvestment={annualInvestment}
+        expectedReturn={expectedReturn}
+        duration={duration}
+      />
     </>
   );
 }
---
+++ investment_calc/src/util/Result.jsx
@@ -0,0 +1,24 @@
+export default function Result() {
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <td>Year</td>
+          <td>Investment Value</td>
+          <td>Interest</td>
+          <td>Total Interest</td>
+          <td>Invested Capital</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
