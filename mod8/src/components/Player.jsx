7b0637f8a8dff57edc9c2fa04de91017756ed43f
hoanhua14 <huasohoan@gmail.com>
2025-01-27 13:09:38 -0800

    added 2 ways binding to name field

--- mod8/src/components/Player.jsx
+++ mod8/src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleClick = () => {
+    setSubmitted(true);
+  };
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? { enteredPlayerName } : "unknown"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          value={enteredPlayerName}
+          onChange={handleNameChange}
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
