d14673cc26a49cbc3832b1824a18dd964312b6a9
hoanhua14 <huasohoan@gmail.com>
2025-01-28 16:46:25 -0800

    added timer useRef

--- mod8/src/components/TimerChallenge.jsx
+++ mod8/src/components/TimerChallenge.jsx
@@ -1,14 +1,34 @@
+import { useRef, useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  function handleStart() {
+    setTimerStarted(true);
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, 1000 * targetTime);
+  }
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>lost</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "stop" : "start"} Challenge
+        </button>
       </p>
-      <p className=""> Time running?</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {" "}
+        {timerStarted ? "running" : "timer inactive"}
+      </p>
     </section>
   );
 }
