5fc3324cdb727cff49dc61a1e067aa0ff52bb48f
hoanhua14 <huasohoan@gmail.com>
2025-01-28 16:15:48 -0800

    created TimerChallenge reusable component

--- mod8/src/components/Player.jsx
+++ mod8/src/components/Player.jsx
@@ -6,6 +6,7 @@ export default function Player() {
 
   const handleClick = () => {
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
   };
   return (
     <section id="player">
