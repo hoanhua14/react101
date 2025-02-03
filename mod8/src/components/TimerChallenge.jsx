45c0c182c75437bb96f260486d8345af9d54c3fd
hoanhua14 <huasohoan@gmail.com>
2025-02-02 20:44:19 -0800

    fixed dialog auto mounts when page first renders

--- mod8/src/components/TimerChallenge.jsx
+++ mod8/src/components/TimerChallenge.jsx
@@ -22,7 +22,6 @@ export default function TimerChallenge({ title, targetTime }) {
 
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>lost</p>}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
