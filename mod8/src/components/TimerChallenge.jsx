8180f5de75b20630584e9d623b24b8df978e3304
hoanhua14 <huasohoan@gmail.com>
2025-02-06 16:37:48 -0800

    changed state updating to track time remaining

--- mod8/src/components/TimerChallenge.jsx
+++ mod8/src/components/TimerChallenge.jsx
@@ -4,17 +4,21 @@ import ResultModal from "./ResultModal";
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    setTimeRemaining(targetTime * 1000);
+    dialog.current.open();
+  }
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.open();
-    }, 1000 * targetTime);
-    setTimerStarted(true);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
   }
   function handleStop() {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   }
   return (
     <>
@@ -26,13 +30,13 @@ export default function TimerChallenge({ title, targetTime }) {
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "stop" : "start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "stop" : "start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
+        <p className={timerIsActive ? "active" : undefined}>
           {" "}
-          {timerStarted ? "running" : "timer inactive"}
+          {timerIsActive ? "running" : "timer inactive"}
         </p>
       </section>
     </>
