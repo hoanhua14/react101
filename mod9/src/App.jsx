686753617bbdc249fa36a40fb167329d4b3b079a
hoanhua14 <huasohoan@gmail.com>
2025-03-05 17:02:15 -0800

    added styling to Input component

--- mod9/src/App.jsx
+++ mod9/src/App.jsx
@@ -2,7 +2,7 @@ import NewProject from "./components/NewProject";
 import ProjectSideBar from "./components/ProjectSideBar";
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8 ">
       <ProjectSideBar />
       <NewProject />
     </main>
--- mod9/src/components/Input.jsx
+++ mod9/src/components/Input.jsx
@@ -1,8 +1,16 @@
-export default function Input({ isTextArea, name }) {
+export default function Input({ isTextArea, name, ...props }) {
+  const style =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
   return (
-    <p>
-      <label>{name}</label>
-      {isTextArea ? <textarea /> : <input></input>}
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {name}
+      </label>
+      {isTextArea ? (
+        <textarea className={style} {...props} />
+      ) : (
+        <input className={style} {...props}></input>
+      )}
     </p>
   );
 }
