852b6e88ec108d7279db47961a1b2329cda5bfe1
hoanhua14 <huasohoan@gmail.com>
2025-03-16 16:50:35 -0700

    sund

--- mod9/src/App.jsx
+++ mod9/src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import ProjectSideBar from "./components/ProjectSideBar";
 function App() {
+  const [projectSelected, setProjectSelected] = useState(false);
   return (
     <main className="h-screen my-8 flex gap-8 ">
       <ProjectSideBar />
